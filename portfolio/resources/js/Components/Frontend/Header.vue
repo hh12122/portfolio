37a0a91e34260da353e8e8319656e83bf0611932
hocine amrane <hocine.amrane@tech-instinct.com>
2025-09-22 21:14:25 +0100

    add logo

--- portfolio/resources/js/Components/Frontend/Header.vue
+++ portfolio/resources/js/Components/Frontend/Header.vue
@@ -1,12 +1,11 @@
 <template>
-<nav  class=" w-full fixed z-20 border-gray-200 px-2 sm:px-4 py-2.5 rounded" :class="{
+<nav  class=" w-full fixed z-20 border-gray-200  rounded" :class="{
     'bg-light-primary dark:bg-dark-primary':scrollBg,
     'bg-white data:bg-slate-800':!scrollBg
 }">
-  <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+  <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto">
     <a href="" class="flex items-center space-x-3 rtl:space-x-reverse">
-        <img src="https://flowbite.com/docs/images/logo.svg" class="h-8" alt=" Logo" />
-        <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Menu</span>
+        <img src="http://localhost:8000/img/logo.png" class="h-30 w-20" alt=" Logo" />
     </a>
     <button @click="showMobileMenu = !showMobileMenu" data-collapse-toggle="navbar-default" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
         <span class="sr-only">Open main menu</span>
